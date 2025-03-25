07f45fa58e682dc4b2f56d5ef682db7e0456413b
Valerie Fuchs <valeriefuchs@Valeries-MacBook-Pro.local>
2025-03-25 11:14:45 +0100

    Refactor in components

---
+++ components/CallOut.js
@@ -0,0 +1,31 @@
+export default function CallOut() {
+  return (
+    <svg viewBox="0 0 776 556" xmlns="http://www.w3.org/2000/svg">
+      <g fill="none" fillRule="evenodd">
+        <text
+          transform="rotate(6 352.545 334.644)"
+          fontFamily="Riposte-Light, Riposte"
+          fontSize="65.592"
+          fontWeight="300"
+          letterSpacing="-.031"
+          fill="#000"
+        >
+          <tspan x="263.329" y="203.238">
+            OMG YASS
+          </tspan>{" "}
+          <tspan x="318.019" y="268.038">
+            THERE{" "}
+          </tspan>{" "}
+          <tspan x="281.43" y="332.838">
+            IS A LIST!
+          </tspan>
+        </text>
+        <path
+          stroke="#0000FF"
+          strokeWidth="5.76"
+          d="M537.04 129.4L433.36 7l-51.84 95.04-146.88-74.88L254.8 129.4 10 117.88l171.36 122.4L63 296h171.64l20.16 163 99.36-97.76 146.88 188.64 57.6-195.84 210.24 82.08-123.84-195.84L768.88 7z"
+        />
+      </g>
+    </svg>
+  );
+}
